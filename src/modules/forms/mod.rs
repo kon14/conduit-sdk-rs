c7f7042643f1f60fec15d1707eaef2f80370e64c
Konstantinos Feretos <konferetos@tutanota.com>
2024-09-07 12:00:32 +0300

    feat(forms): module ctor

--- src/modules/forms/mod.rs
+++ src/modules/forms/mod.rs
@@ -9,6 +9,18 @@ pub struct FormsModule {
     pub health_client: HealthClient,
 }
 
+impl FormsModule {
+    pub fn new(grpc_address: GrpcAddress, channel: Channel) -> Self {
+        let module_client = ConduitModuleClient::new(channel.clone());
+        let health_client = HealthClient::new(channel);
+        FormsModule {
+            grpc_address,
+            _module_client: module_client,
+            health_client,
+        }
+    }
+}
+
 impl std::fmt::Debug for FormsModule {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Forms @ {:?}", self.grpc_address)
